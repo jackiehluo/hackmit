3592626615d18893c0713f3deeb4d9ebb1f7a091
Jackie Luo <jacquelineluo95@gmail.com>
2015-09-19 18:06:16 -0400

    Add view for index

---
+++ app/app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask, render_template
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    start = datetime.now() - timedelta(weeks=2)
+    data = Person.query.filter(_and(Person.visit_time >= start)).all()
+    return render_template('index.html',
+                            data=data)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0")
